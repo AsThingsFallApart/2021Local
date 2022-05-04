e5a18c8b7cc7044b8c425e5cb18a36300c8497c4
ATFA <asthingsfallapart@gmail.com>
2022-05-04 15:17:59 -0400

    add newline character at end of output

--- solutions_ATFA/brownie.cpp
+++ solutions_ATFA/brownie.cpp
@@ -1,138 +1,52 @@
-// brownie.cpp
-
-// every program can be abstracted into three parts:
-// 1. input
-// 2. black box
-// 3. output
-
-// input is put into some black box and then output comes out
-// writing a program involves visualizing the code for each part
-// 1. how is the input formatted?
-// 1a. following the input format, what code structures can be used to organize the input?
-//    e. g. getline native function, cin object, extraction operator, string data type, arrays, vectors
-// 2. what logic is requested?
-// 2a. what code structures can be used to represent the requested logic?
-//    e.g. if statements, loops, arithmetic operators, int data types, iterator variables
-// 3. how is the output formatted?
-// 3a. following the output format, what code structures can be used to realize that format?
-//    e.g. cout object, insertion operator, endl object
-
-/* plan to parse the input: */
-// input lines:
-// 1. "[# students] [# brownies]"
-// 2. "[m number of groups]"
-// 2a. int i = 0;
-// 2 + m. [number of students in group m - (m - i); i++; ...group m - (m - (m-1))]
-
-/* plan to implement dr. O dividing brownies logic: */
-// if([# of brownies remaining] <= [# of students in group]) {
-//    // [looping code to divide brownies (multiply by 2) until [# brownies] > [# students in group]]
-// }
-
-/* plan to format output: */
-// print m number of lines
-// each line has two integers:
-// "[# of students in a group] [# of brownies left AFTER each student grabs one brownie]"
-// grabbing occurs after brownie multiplication, thus:
-// numBrowniesRemaining = numBrowniesAfterMultiplication - numStudentsInGroup;
-
 #include <iostream>
 using namespace std;
 
+// use the newline character ("\n") instead of the end line manipulator for basic printing
+// the end line manipulator has additional behavior to FLUSH the output stream
+// FLUSHING the output stream incurs an overhead and might cause delays on some machines
+// in other words: in most cases, using the newline character to start a newline is faster
+
 int main() {
-  // what happens in int main...? Everything above, I guess.
-  // do I not want to do this...?
+  int numStudents, numBrownies, numGroups;
 
-  int numStudents;
-  int numBrownies;
-  string inputLine;
+  // cout << "Please enter brownie scenario:\n";
 
-  getline(cin, inputLine);
-  cout << endl << "inputLine: " << inputLine << endl;
+  cin >> numStudents >> numBrownies >> numGroups;
 
-  int i = 0;
+//  cout << "\nnumStudents: " << numStudents << "\nnumBrownies: " << numBrownies;
+//  cout << "\nnumGroups: " << numGroups;
+
+  int i, numStudentsInGroupM = 0;
+  string output = "";
   string temp = "";
 
-  for(char c : inputLine) {
-    i++;
-
-    if(c == ' ') {
-      numStudents = stoi(temp);
-      temp = "";
-    }
-    else if(i == inputLine.length()) {
-      temp += c;
-      numBrownies = stoi(temp);
-    }
-    else {
-      temp += c;
-    }
-  }
-
-  cout << endl << "numStudents: " << numStudents << endl;
-  cout << "numBrownies: " << numBrownies << endl << endl;
-
-  int numGroups;
-
-  getline(cin, inputLine);
-  cout << "inputLine: " << inputLine << endl;
-  numGroups = stoi(inputLine);
-
-  cout << "numGroups: " << numGroups << endl << endl;
-
-  // put number of people in each group in a container
-  int groups[numGroups];
+  /* the algorithmic black box's behavior DEPENDS ON the output format */
 
   for(i = 0; i < numGroups; i++) {
-    if(i != (numGroups - 1)) {
-      getline(cin, inputLine);
-      cout << "inputLine: " << inputLine << endl;
-      groups[i] = stoi(inputLine);
-      cout << "groups[" << i << "]: " << groups[i] << endl;
-    }
-    else {
-      getline(cin, inputLine);
-      cout << "inputLine: " << inputLine << endl;
-      groups[i] = stoi(inputLine);
-      cout << "groups[" << i << "]: " << groups[i] << endl;
-      cin.ignore();
-      cout << "inputLine: " << inputLine << endl;
-    }
+      cin >> numStudentsInGroupM;
+
+      while(numBrownies <= numStudentsInGroupM) {
+        numBrownies *= 2;
+      }
+
+      numBrownies = numBrownies - numStudentsInGroupM;
+//      cout << "numStudentsInGroupM: " << numStudentsInGroupM << "\n";
+//      cout << "numBrownies: " << numBrownies << "\n";
+      temp = to_string(numStudentsInGroupM) + " " + to_string(numBrownies);
+
+      output += temp;
+      if(i < numGroups - 1) {
+        output += "\n";
+      }
+
+//      cout << "\noutput: \"" << output << "\"\n";
   }
 
-  // this isn't efficient but whatever...
+  cout << output << "\n";
+
+//  for(i = 0; i < numGroups; i++) {
+//    cout << "groups[" << i << "]: " << groups[i] << "\n";
+//  }
 
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
